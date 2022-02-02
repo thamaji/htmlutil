748fa248bce6e52303a3897c7d90bbe3f0fd2395
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2022-02-03 01:23:59 +0900

    ジェネリクス対応

--- selection.go
+++ selection.go
@@ -70,6 +70,41 @@ func (selection Selection) Parents() Selection {
 	return results
 }
 
+func (selection Selection) AppendPrev(nodes ...*html.Node) {
+	for _, selectionNode := range selection {
+		for _, node := range nodes {
+			node := *node
+			node.NextSibling = selectionNode
+			if prev := selectionNode.PrevSibling; prev != nil {
+				prev.NextSibling = selectionNode
+			}
+			selectionNode.PrevSibling = &node
+		}
+	}
+}
+
+func (selection Selection) AppendNext(nodes ...*html.Node) {
+	for _, selectionNode := range selection {
+		for i := range nodes {
+			node := *nodes[i]
+			node.PrevSibling = selectionNode
+			if next := selectionNode.NextSibling; next != nil {
+				next.PrevSibling = &node
+			}
+			selectionNode.NextSibling = &node
+		}
+	}
+}
+
+func (selection Selection) AppendChild(nodes ...*html.Node) {
+	for _, selectionNode := range selection {
+		for i := range nodes {
+			node := *nodes[i]
+			selectionNode.AppendChild(&node)
+		}
+	}
+}
+
 func (selection Selection) Contains(selector Selector) bool {
 	return selection.Find(selector).Len() > 0
 }
@@ -148,8 +183,8 @@ func (selection Selection) Filter(selector Selector) Selection {
 	return result
 }
 
-func (selection Selection) MapBool(f func(Selection) (bool, error)) ([]bool, error) {
-	result := make([]bool, 0, len(selection))
+func MapSelection[T any](selection Selection, f func(Selection) (T, error)) ([]T, error) {
+	result := make([]T, 0, len(selection))
 
 	for _, node := range selection {
 		val, err := f(Selection([]*html.Node{node}))
@@ -161,304 +196,88 @@ func (selection Selection) MapBool(f func(Selection) (bool, error)) ([]bool, err
 	}
 
 	return result, nil
+}
+
+func (selection Selection) MapBool(f func(Selection) (bool, error)) ([]bool, error) {
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapString(f func(Selection) (string, error)) ([]string, error) {
-	result := make([]string, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapByte(f func(Selection) (byte, error)) ([]byte, error) {
-	result := make([]byte, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapBytes(f func(Selection) ([]byte, error)) ([][]byte, error) {
-	result := make([][]byte, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapRune(f func(Selection) (rune, error)) ([]rune, error) {
-	result := make([]rune, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapRunes(f func(Selection) ([]rune, error)) ([][]rune, error) {
-	result := make([][]rune, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapInt(f func(Selection) (int, error)) ([]int, error) {
-	result := make([]int, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapInt8(f func(Selection) (int8, error)) ([]int8, error) {
-	result := make([]int8, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapInt16(f func(Selection) (int16, error)) ([]int16, error) {
-	result := make([]int16, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapInt32(f func(Selection) (int32, error)) ([]int32, error) {
-	result := make([]int32, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapInt64(f func(Selection) (int64, error)) ([]int64, error) {
-	result := make([]int64, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapUint(f func(Selection) (uint, error)) ([]uint, error) {
-	result := make([]uint, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapUint8(f func(Selection) (uint8, error)) ([]uint8, error) {
-	result := make([]uint8, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapUint16(f func(Selection) (uint16, error)) ([]uint16, error) {
-	result := make([]uint16, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapUint32(f func(Selection) (uint32, error)) ([]uint32, error) {
-	result := make([]uint32, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapUint64(f func(Selection) (uint64, error)) ([]uint64, error) {
-	result := make([]uint64, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapUintptr(f func(Selection) (uintptr, error)) ([]uintptr, error) {
-	result := make([]uintptr, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapFloat32(f func(Selection) (float32, error)) ([]float32, error) {
-	result := make([]float32, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapFloat64(f func(Selection) (float64, error)) ([]float64, error) {
-	result := make([]float64, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapComplex64(f func(Selection) (complex64, error)) ([]complex64, error) {
-	result := make([]complex64, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
 
 func (selection Selection) MapComplex128(f func(Selection) (complex128, error)) ([]complex128, error) {
-	result := make([]complex128, 0, len(selection))
-
-	for _, node := range selection {
-		val, err := f(Selection([]*html.Node{node}))
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, val)
-	}
-
-	return result, nil
+	return MapSelection(selection, f)
 }
